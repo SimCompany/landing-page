fc7572015b691ebaff4fddbabefdcdcc0c56ab3e
Matteus Isaque <matteus.isaque28@gmail.com>
2023-03-06 18:08:56 -0300

    tag-manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
   useEffect(() => {
     const tagManagerArgs = {
-      gtmId: 'GTM-TFCSLRJ'
+      gtmId: 'GTM-PQMG35C'
     }
 
     TagManager.initialize(tagManagerArgs)
