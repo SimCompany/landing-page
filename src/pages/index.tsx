c04a34fabe7d97c7ae7278a60a0d03d8ee4703eb
SimCompany <112415745+SimCompany@users.noreply.github.com>
2023-05-25 13:49:32 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,7 +49,7 @@ export default function Index() {
 
     e.preventDefault()
     const data = {
-    emailTo: ['luana@simcompany.com.br'],
+    emailTo: ['luana@simcompany.com.br', 'hubner@simcompany.com.br', 'edgard@simcompany.com.br', 'provisorio@simcompany.com.br'],
     title: 'Captação de leads por campanha Simcompany',
       domain: 'https://camp.simcompany.com.br/pabx',
       companyName,
